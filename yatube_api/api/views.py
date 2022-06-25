432c20738de4b7604d8714ffd14e5defcf5e2140
Бернова Анна <bast33@yandex.ru>
2022-06-25 12:02:10 +0300

    main structure

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets 
 
-# Create your views here.
+from posts.models import Post, Group, Comment
+from .serializers import PostSerializer,  GroupSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer 
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer 
+    
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer     
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,9 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from api.views import PostViewSet, GroupViewSet, CommentViewSet
+
+router = DefaultRouter()
+
+router.register('api/v1/posts', PostViewSet)
+router.register('api/v1/groups', GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
 
 
